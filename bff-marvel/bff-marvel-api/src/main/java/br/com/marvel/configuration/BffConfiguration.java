36f094757e20ce09814a7bc48bc9049a609c6f0e
Thomas da Costa <thomasdacosta@gmail.com>
2022-02-02 12:55:57 -0300

    feat: incluindo o asyn nas imagens da aplicação

---
+++ bff-marvel/bff-marvel-api/src/main/java/br/com/marvel/configuration/BffConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.marvel.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class BffConfiguration {
+	
+    @Bean(name="processExecutor")
+    public TaskExecutor workExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("Async-");
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        threadPoolTaskExecutor.setMaxPoolSize(10);
+        threadPoolTaskExecutor.setQueueCapacity(600);
+        threadPoolTaskExecutor.afterPropertiesSet();
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }	
+
+}
