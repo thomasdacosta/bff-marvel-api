b2231cfb9aeb92ad0b91b8bdb1a170f538244304
Thomás da Costa <thomasdacosta@gmail.com>
2022-06-12 18:49:13 -0300

    feat: aws cdk do projeto

--- 02-aws/cli/aws_localstack.bat
+++ 02-aws/cli/aws_localstack.bat
@@ -25,3 +25,6 @@ aws --endpoint http://localhost:4566 --profile localstack sqs create-queue --que
 echo ### Criando Queue(Standard) no SNS do LocalStack...
 aws --endpoint http://localhost:4566 --profile localstack sns create-topic --name marvelThumbnailImageNotification
 aws --endpoint http://localhost:4566 --profile localstack sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:marvelThumbnailImageNotification --protocol sqs --notification-endpoint http://localhost:4566/queue/marvelThumbnailImageQueue
+
+echo ### Criando Stack do CloudFormation
+echo aws --endpoint http://localhost:4566 --profile localstack cloudformation create-stack --stack-name marvelStack --template-body file://bff-marvel-cdk_localstack.yml
